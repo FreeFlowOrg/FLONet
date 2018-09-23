0696f474f819e8391a5f8685d5df94695051d1c0
rahulkumaran <rahuldravid313@gmail.com>
2018-09-23 16:16:10 +0530

    modularised wiki code

--- wiki.py
+++ wiki.py
@@ -15,4 +15,35 @@ def hackathon_search(country):
 
 	soup = BeautifulSoup(response.content, "html.parser")
 
-	return soup
+	comm_title_box = soup.find_all("a", attrs={"class":"ht-eb-card__title"})
+
+	comm_date_box = soup.find_all("div", attrs={"class":"ht-eb-card__left"})
+
+	idt_title_box = soup.find_all("a", attrs={"class":"ht-idt-card__title"})
+
+	idt_date_box = soup.find_all("div", attrs={"class":"ht-idt-card__date"})
+
+	titles = []
+	dates = []
+	links = []
+	for a in comm_title_box:
+		titles += [a.text]
+		links += [a.get("href")]
+
+	for a in idt_title_box:
+		titles += [a.text]		
+		links += [a.get("href")]
+
+	for a in comm_date_box:
+		dates += [a.text]
+
+	for a in idt_date_box:
+		dates += [a.text]
+
+	return titles, dates, links
+
+
+
+'''a, b, c = hackathon_search("india")
+
+print(a, b, c)'''
